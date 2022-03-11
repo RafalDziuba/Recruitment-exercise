69c97639a94f313dfdb9a3fe0cc0bae8195d76c8
RafalDziuba <72468641+RafalDziuba@users.noreply.github.com>
2022-03-11 22:18:57 +0100

    Calc refactor

--- calc.js
+++ calc.js
@@ -1,22 +1,30 @@
 {
   const resultContent = document.querySelector(".calculator__result");
   const inputs = document.querySelectorAll(".calculator__input");
+  let result;
+
+
+  const mathOperation = (a, b) => {
+    result = (a * b - (a * b) / 5) * 12;
+  };
+
+  const changeResultContent = () => {
+    resultContent.textContent = result.toFixed(2) + "zł";
+    resultContent.style.color = "#ffb001";
+    resultContent.style.opacity = "1";
+  };
+
   inputs.forEach((input) => {
     input.addEventListener("input", (event) => {
       let glassBox = document.querySelector("#glass-boxes").value;
       let glassExport = document.querySelector("#glass-export").value;
       parseInt(glassBox);
       parseInt(glassExport);
-      const mathOperation = (a, b) => {
-        let result;
-        result = (a * b - (a * b) / 5) * 12;
-
-        resultContent.textContent = result.toFixed(2) + 'zł';
-        resultContent.style.color = '#ffb001';
-        resultContent.style.opacity = '1';
-      };
 
       mathOperation(glassBox, glassExport);
+
+      changeResultContent();
     });
   });
+ 
 }
